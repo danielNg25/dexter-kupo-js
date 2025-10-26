46f03a49bd9ac1742da7c9c83bad93ebf60e93e8
danielNg25 <ndtr2000@gmail.com>
2025-10-27 00:42:13 +0700

    Remove debug log for jsonDatum in ChadSwap class

--- src/dex/chadswap.ts
+++ src/dex/chadswap.ts
@@ -129,7 +129,6 @@ export class ChadSwap {
 
             return parameters;
         } catch (error) {
-            console.log('Json Datum', jsonDatum);
             try {
                 const builder: DefinitionBuilder =
                     await new DefinitionBuilder().loadDefinition(
